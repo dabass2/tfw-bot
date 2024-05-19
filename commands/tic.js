e892321c9c508b570bafa6e192848fe4068050c0
Daniel Bass <danielbass123456@gmail.com>
2024-05-19 14:09:17 -0500

    feat: Re-do id system, mark required options, pretty up embed

--- commands/tic.js
+++ commands/tic.js
@@ -6,111 +6,112 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName("tic")
     .setDescription("Inner circle best circle")
-    .addSubcommandGroup(movies =>
-        movies
-            .setName("movies")
-            .setDescription("TIC movie list")
-            .addSubcommand(list => 
-                list
-                    .setName("list")
-                    .setDescription("Get list of requested movies")
+    .addSubcommandGroup((movies) =>
+      movies
+        .setName("movies")
+        .setDescription("TIC movie list")
+        .addSubcommand((list) =>
+          list.setName("list").setDescription("Get list of requested movies")
+        )
+        .addSubcommand((add) =>
+          add
+            .setName("add")
+            .setDescription("Add movie")
+            .addStringOption((name) =>
+              name
+                .setName("name")
+                .setDescription("The name of the movie")
+                .setRequired(true)
             )
-            .addSubcommand(add => 
-                add
-                    .setName("add")
-                    .setDescription("Add movie")
-                    .addStringOption(name =>
-                        name
-                            .setName("name")
-                            .setDescription("The name of the movie")
-                            .setRequired(true)
-                    )
+        )
+        .addSubcommand((vote) =>
+          vote
+            .setName("vote")
+            .setDescription("Vote to watch or not watch a movie")
+            .addNumberOption((vote) =>
+              vote
+                .setName("score")
+                .setDescription("Positive for yes, negative for no")
+                .setRequired(true)
             )
-            .addSubcommand(vote =>
-                vote
-                    .setName("vote")
-                    .setDescription("Vote to watch or not watch a movie")
-                    .addNumberOption(vote =>
-                        vote
-                            .setName("score")
-                            .setDescription("Positive for yes, negative for no")
-                    )
-                    .addNumberOption(id =>
-                        id
-                            .setName("id")
-                            .setDescription("The id of the movie to vote on")
-                    )
+            .addNumberOption((id) =>
+              id
+                .setName("id")
+                .setDescription("The id of the movie to vote on")
+                .setRequired(true)
             )
-            .addSubcommand(rm =>
-                rm
-                    .setName("rm")
-                    .setDescription("Remove a movie from the queue")
-                    .addNumberOption(id =>
-                        id
-                            .setName("id")
-                            .setDescription("The id of the movie to remove")
-                    )
+        )
+        .addSubcommand((rm) =>
+          rm
+            .setName("rm")
+            .setDescription("Remove a movie from the queue")
+            .addNumberOption((id) =>
+              id
+                .setName("id")
+                .setDescription("The id of the movie to remove")
+                .setRequired(true)
             )
+        )
     ),
   async execute(interaction) {
     const ticJson = JSON.parse(fs.readFileSync("../tic.json"));
-    console.log(ticJson);
 
     const sub_command = interaction.options.getSubcommand();
 
     const id = interaction.options.getNumber("id");
     const vote = interaction.options.getNumber("score");
-    const movieName = interaction.options.getString("name")
+    const movieName = interaction.options.getString("name");
 
-    let embed;
+    let embed = new EmbedBuilder();
+    embed.setTimestamp(new Date());
+    embed.setColor(0xb085d4);
+
     if (sub_command === "list") {
-        embed = new EmbedBuilder()
-            .setDescription("TIC Movie Queue")
-            .setTimestamp(new Date());
-        ticJson.movies.sort((a,b) => a.score > b.score ? -1 : 1).map(movie => {
-            embed.addFields({
-                name: "Movie Name",
-                value: `${movie.id}: ${movie.name} - Votes: ${movie.score}`
-            });
-        })
+      embed.setDescription("**TIC Movie Queue**");
     } else if (sub_command === "add") {
-        ticJson.movies.push({
-            id: ticJson.movies?.length ?? 0,
-            name: movieName,
-            requestor: interaction.member.user.id,
-            dateAdded: new Date(),
-            score: 1
-        })
-        fs.writeFileSync("../tic.json", JSON.stringify(ticJson));
-        embed = new EmbedBuilder()
-            .setDescription("TIC Movie Queue")
-            .setDescription(`"${movieName}" added to the queue`)
-            .setTimestamp(new Date());
+      ticJson.movies.push({
+        name: movieName,
+        requestor: interaction.member.user.id,
+        dateAdded: new Date(),
+        score: 1,
+      });
+
+      fs.writeFileSync("../tic.json", JSON.stringify(ticJson));
+      embed.setDescription(`"${movieName}" added to the queue`);
     } else if (sub_command === "rm") {
-        const oldMovie = ticJson.movies?.splice(id, 1)
+      if (id > 0 || id < ticJson.movies.length) {
+        ticJson.movies
+          ?.sort((a, b) => (a.score > b.score ? -1 : 1))
+          .splice(id - 1, 1);
         fs.writeFileSync("../tic.json", JSON.stringify(ticJson));
-        embed = new EmbedBuilder()
-            .setDescription("TIC Movie Queue")
-            .setDescription(`${oldMovie?.name ?? "Movie"} removed from the queue`)
-            .setTimestamp(new Date());
+      }
+
+      embed.setDescription("**Movie removed from the queue**");
     } else if (sub_command === "vote") {
-        const movie = ticJson.movies?.at(id);
-        if (movie) {
-            movie.score += vote ?? 1 > 0 ? 1 : -1
-        }
-        fs.writeFileSync("../tic.json", JSON.stringify(ticJson));
-        embed = new EmbedBuilder()
-            .setDescription("TIC Movie Queue")
-            .setDescription("Vote recorded")
-            .setTimestamp(new Date());
+      const movie = ticJson.movies?.at(id - 1);
+      if (movie) {
+        movie.score += vote ?? 1 > 0 ? 1 : -1;
+      }
+
+      fs.writeFileSync("../tic.json", JSON.stringify(ticJson));
+      embed.setDescription("**Vote recorded**");
     } else {
-        embed = new EmbedBuilder()
-            .setDescription("what?")
-            .setTimestamp(new Date());
+      embed.setDescription("__what?__");
     }
 
+    ticJson.movies
+      .sort((a, b) => (a.score > b.score ? -1 : 1))
+      .map((movie, idx) => {
+        embed.addFields({
+          name: "Movie Name",
+          value: `[#${idx + 1}]: "**${movie.name}**" - Votes: **${
+            movie.score
+          }** (Requestor: <@${movie.requestor}>)`,
+        });
+      });
+
     await interaction.reply({
-      embeds: [embed]
+      embeds: [embed],
     });
   },
 };
